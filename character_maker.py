385df1e40a2805810424e179a76045781cbeef1b
Justin Worrell <jworrell@gmail.com>
2020-07-26 10:25:04 -0500

    initial commit

--- character_maker.py
+++ character_maker.py
@@ -2,27 +2,65 @@
 Simple API for rolling new characters
 """
 
-import random
 import json
+import random
+from dataclasses import dataclass, asdict
 from typing import Dict, List, Any, Optional, Tuple
 
 import yaml
 
 
-def roll_stat() -> int:
-    return sum(random.randint(1, 6) for _ in range(3))
+@dataclass
+class CharacterStats:
+    strength: int
+    dexterity: int
+    constitution: int
+    intelligence: int
+    wisdom: int
+    charisma: int
+
+    @staticmethod
+    def roll_stat() -> int:
+        return sum(random.randint(1, 6) for _ in range(3))
+
+    @staticmethod
+    def roll_stats() -> 'CharacterStats':
+        return CharacterStats(
+            strength=CharacterStats.roll_stat(),
+            dexterity=CharacterStats.roll_stat(),
+            constitution=CharacterStats.roll_stat(),
+            intelligence=CharacterStats.roll_stat(),
+            wisdom=CharacterStats.roll_stat(),
+            charisma=CharacterStats.roll_stat(),
+        )
 
 
-def roll_stats() -> Dict[str, int]:
-    return {
-        "Strength": roll_stat(),
-        "Dexterity": roll_stat(),
-        "Constitution": roll_stat(),
-        "Intelligence": roll_stat(),
-        "Wisdom": roll_stat(),
-        "Charisma": roll_stat(),
-        "Reroll": roll_stat(),
-    }
+@dataclass
+class CharacterChoice:
+    choice: str
+    subchoice: Optional[str]
+
+
+@dataclass
+class Character:
+    race: CharacterChoice
+    background: CharacterChoice
+    _class: CharacterChoice
+    reroll: int
+    rolled_stats: CharacterStats
+    suggested_stats: Optional[CharacterStats]
+
+    def suggest_stats(self):
+        rolled_stats = asdict(self.rolled_stats)
+
+        worst_stat = min((v, k) for k, v in rolled_stats.items())
+
+        print(rolled_stats, worst_stat, self.reroll)
+
+        if worst_stat[0] < self.reroll:
+            rolled_stats[worst_stat[1]] = self.reroll
+
+        best_stat = max((v, k) for k, v in rolled_stats.items())
 
 
 class RandomTable:
@@ -57,19 +95,27 @@ class RandomTable:
 
         return True
 
-    def choose(self) -> List['RandomTableOption']:
+    def _choose(self) -> List['RandomTableOption']:
         roll = random.randint(1, self.die_size)
 
         for choice in self.choices:
             if choice.low <= roll <= choice.high:
-                chain = [choice]
+                choice_chain = [choice]
 
                 if choice.subtable:
-                    chain += choice.subtable.choose()
+                    choice_chain += choice.subtable._choose()
 
-                return chain
+                return choice_chain
 
         raise ValueError("Failed to find choice, this shouldbn't happen")
+
+    def choose(self) -> CharacterChoice:
+        choice_chain = self._choose()
+
+        return CharacterChoice(
+            choice=choice_chain[0].value,
+            subchoice=None if len(choice_chain) < 2 else choice_chain[1].value,
+        )
 
 
 class RandomTableOption:
@@ -87,17 +133,6 @@ class RandomTableOption:
         self.high = high
         self.value = value
         self.subtable = subtable
-
-
-def format_choice(choice: List[RandomTableOption]) -> str:
-    if len(choice) == 1:
-        return f"{choice[0].value}"
-
-    elif len(choice) == 2:
-        return f"{choice[0].value} ({choice[1].value})"
-
-    else:
-        raise ValueError("Don't know how to format choice")
 
 
 def get_base_headers(content_length: int) -> Dict[str, str]:
@@ -125,12 +160,20 @@ BACKGROUND_TABLE, CLASS_TABLE, RACE_TABLE = lambda_init()
 
 
 def lambda_handler(event: Dict[str, Any], context: Dict[str, Any]) -> Dict[str, Any]:
-    result = json.dumps({
-        "race": format_choice(RACE_TABLE.choose()),
-        "background": format_choice(BACKGROUND_TABLE.choose()),
-        "class": format_choice(CLASS_TABLE.choose()),
-        "stats": roll_stats(),
-    })
+    rolled_stats = CharacterStats.roll_stats()
+
+    character = Character(
+        race=RACE_TABLE.choose(),
+        background=BACKGROUND_TABLE.choose(),
+        _class=CLASS_TABLE.choose(),
+        reroll=CharacterStats.roll_stat(),
+        rolled_stats=rolled_stats,
+        suggested_stats=None,
+    )
+
+    character.suggest_stats()
+
+    result = json.dumps(asdict(character))
 
     return {
         "statusCode": 200,
